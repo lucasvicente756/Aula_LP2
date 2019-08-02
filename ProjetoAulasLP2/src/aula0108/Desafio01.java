ac85e4c5bf7ecbb25e534d9faf0f31fc3e0a3ee1
Carmem <Carmem@Carmem-PC>
2019-08-02 18:21:41 -0300

    Desafio 01

--- ProjetoAulasLP2/src/aula0108/Desafio01.java
+++ ProjetoAulasLP2/src/aula0108/Desafio01.java
@@ -1,5 +1,28 @@
 package aula0108;
 
 public class Desafio01 {
+public static void main(String[] args) {
+		
+		int[][] matriz = new int [5][5];
+		
+		Random numerosr = new Random();
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				
+				matriz[i][j] = numerosr.nextInt(25);
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				
+				System.out.println(matriz[i][j]+ " ");
+
+			}
+			
+			System.out.println("\n");
+			
+		}
+	}
 
 }
